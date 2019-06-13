e42a58796aa98c79d69bc144a35ec2345f157f2b
Hritik Jain <31235739+CodeMostt@users.noreply.github.com>
2019-06-13 12:24:56 +0530

    Update regressor.py

--- regressor.py
+++ regressor.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Jun 12 21:42:21 2019
+Created on Wed Jun 12 22:14:52 2019
 
 @author: userr
 """
 
-# Kernel PCA
+#Multiple Linear Regression
 
 # Importing the libraries
 import numpy as np
@@ -18,31 +18,17 @@ dataset = pd.read_csv('Admission_Predict_Ver1.1.csv')
 X = dataset.iloc[:, 1:8].values
 y = dataset.iloc[:, 8].values
 
-
-# Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-# Feature Scaling
+
+#feature scaling
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)
-
-# Applying Kernel PCA
 
 
-#Apply PCA
-
-from sklearn.decomposition import PCA
-pca = PCA(n_components= 1 )
-X_train=pca.fit_transform(X_train)
-X_test=pca.transform(X_test)
-explained_variance = pca.explained_variance_ratio_
-
-# Fitting SVR to the dataset
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
@@ -50,7 +36,17 @@ regressor.fit(X_train, y_train)
 # Predicting the Test set results
 y_pred = regressor.predict(X_test)
 
-# Making the Confusion Matrix
+
+#Building optimal model Using Backward Elimination
+import statsmodels.formula.api as sm
+
+#1 ka matrix sabse aage add krna b0 ka cofficient
+X = np.append(arr=np.ones((500,1)).astype(int), values=X, axis=1 )
+
+X_opt=X[:,[0,1,2,3,4,5,6,7]]
+regressor_OLS=sm.OLS(endog=y, exog=X_opt).fit()
+regressor_OLS.summary()
+
 from sklearn.model_selection import cross_val_score
 accuracies = cross_val_score(estimator = regressor, X = X_train, y = y_train, cv = 10)
 accuracies.mean()
@@ -59,10 +55,3 @@ accuracies.std()
 plt.scatter(X_train,y_train,color='red')
 plt.plot(X_train,regressor.predict(X_train),color='blue')
 plt.show()
-
-plt.scatter(X_test,y_test,color='red')
-plt.plot(X_train,regressor.predict(X_train),color='blue')
-plt.show()
-
-
-#88 % accuracy with random forest
